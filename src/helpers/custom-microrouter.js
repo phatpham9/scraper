16b1a6a15162b0f5792ed6fb97b0be1a73eb78a2
phatpham9 <phatpham9@gmail.com>
2017-12-24 10:38:49 +0700

    refactor: move rate limit to custom-microrouter

--- src/helpers/custom-microrouter.js
+++ src/helpers/custom-microrouter.js
@@ -1,6 +1,12 @@
 const { send } = require('micro');
 const { router, get, post, put, del } = require('microrouter');
 const cors = require('micro-cors')();
+const rateLimit = require('micro-ratelimit');
+
+const rateLimitConfig = {
+  limit: 1,     // on request
+  window: 1000, // per one second
+};
 
 const wrap = cb => (path, fn) => {
   const newFn = (req, res) => {
@@ -9,7 +15,7 @@ const wrap = cb => (path, fn) => {
     return fn(req, res);
   };
 
-  return cb(path, cors(newFn));
+  return cb(path, cors(rateLimit(rateLimitConfig, newFn)));
 };
 
 module.exports = {
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 require('dotenv').config();
 
 const { NOT_FOUND } = require('http-status-codes');
-const rateLimit = require('micro-ratelimit');
 
 const { router, get } = require('./helpers/custom-microrouter');
 const { validate } = require('./middleware');
@@ -12,12 +11,7 @@ const notFound = (req, res) => res.send(NOT_FOUND, {
   message: 'Not found',
 });
 
-const rateLimitConfig = {
-  limit: 1,     // on request
-  window: 1000, // per one second
-};
-
 module.exports = router(
-  get('/scrape', rateLimit(rateLimitConfig, validate(scrape))),
+  get('/scrape', validate(scrape)),
   get('/*', notFound),
 );
